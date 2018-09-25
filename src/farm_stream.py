35a6c90ce9547415b06754a9ada18c18ff4cff3e
Christopher Cantrell <topherCantrell@gmail.com>
2018-09-25 14:37:24 -0500

    added waiter

--- src/farm_stream.py
+++ src/farm_stream.py
@@ -1,8 +1,11 @@
 import urllib.request
 import json
+import time
 
 FARM_CHRIS = 'https://io.adafruit.com/api/v2/topher_cantrell/feeds/quantum-farmer-chris/data'
 FARM_GARY = 'https://io.adafruit.com/api/v2/garydion/feeds/quantum-farmer-gary/data'
+
+TIME_BETWEEN_POLLS = 2 # 2 Seconds
 
 with open('credentials.json') as f:
     config = json.load(f)
@@ -11,6 +14,8 @@ post_headers = {
     'X-AIO-Key': config['AIO'], 
     'Content-Type':'application/json'
 }
+
+last_animal = {'id':''}
 
 def get_last_animal(feed_url):
     req = urllib.request.Request(url=feed_url)
@@ -25,6 +30,19 @@ def post_animal(feed_url,animal):
     f = urllib.request.urlopen(req)
     ret =  json.loads(f.read().decode())
     return ret
+
+def wait_for_new_animal(feed_url):
+    global last_animal
+    while True:
+        an = get_last_animal(feed_url)
+        if an['id'] != last_animal['id']:
+            last_animal = an
+            return an
+        time.sleep(TIME_BETWEEN_POLLS)
+        
+print(wait_for_new_animal(FARM_CHRIS))
+
+print(wait_for_new_animal(FARM_CHRIS))
 
 #post_animal(FARM_CHRIS,'Bat')
 #print(get_last_animal(FARM_CHRIS))
