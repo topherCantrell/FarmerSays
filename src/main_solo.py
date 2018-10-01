9bfde6810581d9f398542e04de737c929e380d09
tophe <tophe@DESKTOP-EC8N5HK>
2018-10-01 18:38:38 -0500

    comments and cleans

--- src/main_solo.py
+++ src/main_solo.py
@@ -1,7 +1,26 @@
-import hdw_farmer_says as HDW
-import farm_audio as AUD
 import time
 import threading
+
+import hdw_farmer_says as HDW # Switches and motor
+import farm_audio as AUD      # Audio files
+
+def spin_motor():   
+    # Start fast to get the motor going then fade
+    HDW.set_motor(3000)
+    time.sleep(0.5)
+    HDW.set_motor(1500)
+    time.sleep(1)
+    HDW.set_motor(800)
+    time.sleep(1)
+    HDW.set_motor(None)
+
+def switch_changed(event):    
+    # Two threads: one to manage the audio and one to run the motor.
+    animal = ANIMALS[event]    
+    t = threading.Thread(target=AUD.prompt_and_play,args=(animal,))
+    t.start()
+    t = threading.Thread(target=spin_motor)
+    t.start()     
 
 # Mapping of pins to animal name (for audio files)
 ANIMALS = {
@@ -18,27 +37,11 @@ ANIMALS = {
     HDW.S_2_00   : 'duck', 
     HDW.S_1_00   : 'dog'
 } 
-
-def spin_motor():   
-    # Start fast to get the motor going then fade
-    HDW.set_motor(3000)
-    time.sleep(0.5)
-    HDW.set_motor(1500)
-    time.sleep(1)
-    HDW.set_motor(800)
-    time.sleep(1)
-    HDW.set_motor(None)
-
-def switch_changed(event):    
-    animal = ANIMALS[event]    
-    t = threading.Thread(target=AUD.prompt_and_play,args=(animal,))
-    t.start()
-    t = threading.Thread(target=spin_motor)
-    t.start()     
      
 HDW.init(ANIMALS,switch_changed) 
 
-# We are ready
+# Tell the user we are ready (it takes a few seconds
+# to initialize pygame and load the sounds).
 AUD.play('rooster')
 spin_motor()
 
